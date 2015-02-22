a049c63d6a5630e24982c65cab9672789e91e930
Jung Oh <Jung3o@yahoo.com>
2015-02-22 10:30:49 -0600

    setting up react with elixir

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,5 +27,10 @@
     <!-- Scripts -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/react/0.12.2/react.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/react/0.12.2/JSXTransformer.js"></script>
+
+    @yield('js')
 </body>
 </html>
